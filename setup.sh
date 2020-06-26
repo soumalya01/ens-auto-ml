86c4e10400573660d0c4e633e60b79129c9c4da7
akaanirban <anirban.dash@gmail.com>
2020-06-25 22:00:36 -0400

    Rebased heroku branch and master

--- setup.sh
+++ setup.sh
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-mkdir -p ~/.streamlit/
-echo "
-[general]
-email = \"soumalya01@gmail.com\"
-" > ~/.streamlit/credentials.toml
-
-echo "
-[server]
+mkdir -p ~/.streamlit
+echo "[server]
 headless = true
-enableCORS=false
+enableCORS = false
 port = $PORT
-=======
-mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\n
-port = $PORT\n\
-enableCORS = false\n\
-\n\
->>>>>>> make it better
 " > ~/.streamlit/config.toml
+>>>>>>> Setup
